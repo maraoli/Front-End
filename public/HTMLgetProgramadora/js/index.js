186d72129f4d95c7d1a50764a21a80b07fea4235
maraoli <mara.15oliveira@outlook.com>
2018-03-07 17:27:50 -0300

    cadastro de cara no chão kkk

--- public/HTMLgetProgramadora/js/index.js
+++
@@ -1,9 +0,0 @@
-var linkCadastrar = document.querySelector(''),
-    moduloLogin = document.querySelector('#login'),
-
-linkCadastrar.addEventListener('click', 
-    function(event){
-        moduloLogin.classList.toggle('modal-close');
-    }   
-);
-    
