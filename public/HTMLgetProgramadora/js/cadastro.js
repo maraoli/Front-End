5e8bc4452bfbbb4506812929aa9a731dec509234
maraoli <mara.15oliveira@outlook.com>
2018-03-13 09:08:51 -0300

    arrumando input radio

--- public/HTMLgetProgramadora/js/cadastro.js
+++ public/HTMLgetProgramadora/js/cadastro.js
@@ -1,12 +1,22 @@
+alert("Oi");
 var secao = document.getElementById('tipo-de-cadastro');
+var radio = document.getElementsByName("check-tipo-de-cadastro");
+
 var template = "";
 //user
-if( value === 0 ){
-
+if(radio[0].checked){
+    template += '<input data-form-group="natural,cadastroPlat" data-form-type="email" data-form-required="" data-form-required-message="Campo obrigatório"'
+    +'data-form-type-message="E-mail inválido. Verifique se digitou corretamente." name="RegistrationForm[email]"'
+    +'id="RegistrationForm_email" type="text" maxlength="40">';
 }
 //cia
 else{
-
+    template += '<input data-form-group="natural,cadastroPlat" data-form-type="email" data-form-required="" data-form-required-message="Campo obrigatório"'
+    +'data-form-type-message="E-mail inválido. Verifique se digitou corretamente." name="RegistrationForm[email]"'
+    +'id="RegistrationForm_email" type="text" maxlength="40">';
 }
+secao.innerHTML = template;
+
+
 
 
