33bd88944cd35b8a9bbc4c2b9dbca872c72b5157
maraoli <mara.15oliveira@outlook.com>
2018-03-13 09:35:36 -0300

    Alt

--- public/HTMLgetProgramadora/js/cadastro.js
+++ public/HTMLgetProgramadora/js/cadastro.js
@@ -1,19 +1,20 @@
 alert("Oi");
-var secao = document.getElementById('tipo-de-cadastro');
+var secao = document.getElementsByName('tipo-de-cadastro');
 var radio = document.getElementsByName("check-tipo-de-cadastro");
+console.log(radio);
 
 var template = "";
 //user
 if(radio[0].checked){
     template += '<input data-form-group="natural,cadastroPlat" data-form-type="email" data-form-required="" data-form-required-message="Campo obrigatório"'
     +'data-form-type-message="E-mail inválido. Verifique se digitou corretamente." name="RegistrationForm[email]"'
-    +'id="RegistrationForm_email" type="text" maxlength="40">';
+    +'id="RegistrationForm_email" type="text" maxlength="40"/>';
 }
 //cia
 else{
     template += '<input data-form-group="natural,cadastroPlat" data-form-type="email" data-form-required="" data-form-required-message="Campo obrigatório"'
     +'data-form-type-message="E-mail inválido. Verifique se digitou corretamente." name="RegistrationForm[email]"'
-    +'id="RegistrationForm_email" type="text" maxlength="40">';
+    +'id="RegistrationForm_email" type="text" maxlength="40"/>';
 }
 secao.innerHTML = template;
 
