ebf87743750c4d4f8bae3792b9e8fec7a3128158
maraoli <mara.15oliveira@outlook.com>
2018-03-14 18:40:25 -0300

    package json OK

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   module: {
     rules: [
         { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        { tesnt: /\.(png|svg|jpg|gif)$/, use: ['file-loader'] }
     ]
   }
 };
