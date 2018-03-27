3fd681621a7814c7465ff9ef84407663cf1adc2b
Mara <mara.15oliveira@outlook.com>
2018-03-27 19:24:28 -0300

    Alt

--- src/actions.js
+++ src/actions.js
@@ -2,14 +2,26 @@ export const LOGA_USUARIO = 'LOGA_USUARIO'
 export const DESLOGA_USUARIO = 'DESLOGA_USUARIO'
 
 data={
-    usuario1={
+    usuario1:{
         logado:true,
-        tipo: usuaria,
+        tipo: "usuaria",
     },
 
-    usuario2={
+    usuario2:{
         logado:true,
-        tipo: empresa,
+        tipo: "empresa",
+    }
+}
+
+export function setLocalStore() {
+    return dispatch => {
+        getNotes()
+            .then(response => dispatch({
+                localStorage.setItem('usuario', JSON.stringify(response.data))
+            }))
+            .catch(error => {
+                console.log('Ocorreu um erro', error)
+            })
     }
 }
 
