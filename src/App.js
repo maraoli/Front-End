1a489fe037fae38e8d4f84508f882743d15d69b0
marina <marinaqueirozmaluta@gmail.com>
2018-04-01 18:25:59 -0300

    Subindo fórum ok e cursos

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import CurriculoProfissional from "./components/pages/usuaria/curriculo/profissi
 import CurriculoAcademico from "./components/pages/usuaria/curriculo/academico";
 import ExibirPerfilUsuaria from "./components/pages/usuaria/exibir_perfil";  
 import EditarPerfilUsuaria from "./components/pages/usuaria/editar_perfil";
-import Forum from "./components/pages/usuaria/forum/index"; 
+import Forum from "./components/pages/forum/index"; 
 
 import NavbarEmpresa from './components/pages/empresa/navbar';
 import IndexEmpresa from './components/pages/empresa/index';
@@ -30,8 +30,7 @@ import Matchs from "./components/pages/empresa/matchs/index";
 import UsuariaMinMatch from "./components/pages/empresa/matchs/usuaria-min-match"; 
 // import Vagas from "./components/vagas_1tela";
 
-//Forum
-import Forum from "./components/pages/forum/index"; 
+
 
 class App extends Component {
   constructor(props) {
@@ -65,7 +64,7 @@ class App extends Component {
               <Route exact path="/academico" component={CurriculoAcademico}/>
               <Route path="/exibir-perfil-usuaria" component={ExibirPerfilUsuaria}/>
               <Route path="/editar-perfil-usuaria" component={EditarPerfilUsuaria}/>
-              <Route path="/forum" component={Forum}/>
+              
               {/*empresa*/}
               <Route path="/empresa" component={IndexEmpresa}/>
               <Route path="/exibir-perfil-empresa" component={ExibirPerfilEmpresa}/>
--- src/components/pages/forum/index.js
+++ src/components/pages/forum/index.js
@@ -4,94 +4,100 @@ import ForumTopic from './forum_topic';
 import ForumTopicCreate from './forum_topic_create';
 
 import {
-    BrowserRouter as Router,
-    Route,
-    Link
+	BrowserRouter as Router,
+	Route,
+	Link
 } from 'react-router-dom'
 
 //FILES TEMP
 import Navbar from '../usuaria/Navbar';
 
 class Forum extends Component {
-    constructor(props){
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.values = [
+		this.values = [
 			{
-				titulo: "título 1",
-				conteudo: "xxxxxxxxxxxxxx",
+				titulo: "Aprender react",
+				conteudo: "Meninas, estou amando o curso de react que estou fazendo! Quem quiser algumas dicas, estou a disposição para ajudar :)",
 				link: "forum/1"
 			},
 			{
-				titulo: "título 2",
-				conteudo: "yyyyyyyyyyyyyyy",
+				titulo: "Aprender react",
+				conteudo: "Meninas, estou amando o curso de react que estou fazendo! Quem quiser algumas dicas, estou a disposição para ajudar :)",
 				link: "forum/2"
 			},
 			{
-				titulo: "titulo 3",
-				conteudo: "zzzzzzzzzzzzzzz",
+				titulo: "Aprender react",
+				conteudo: "Meninas, estou amando o curso de react que estou fazendo! Quem quiser algumas dicas, estou a disposição para ajudar :)",
 				link: "forum/3"
 			}
-	               ]
+		]
 
 
-	this.state = {
-		forum: this.values
-    	}
+		this.state = {
+			forum: this.values
+		}
 
-	this.novoPost = this.novoPost.bind(this);
-    }
-
-
-    listarForum() {
-	return this.state.forum.map(function (item_forum) {
-	    return (
-		 <div>
-	    	     <ForumTopic subject={item_forum.titulo} content={item_forum.conteudo} link={item_forum.link} />
-		     <br />
-		 </div>
-	    ); 
-	})
-    };
+		this.novoPost = this.novoPost.bind(this);
+	}
 
 
-    novoPost(post) {
-        //console.log('TITULO: ',post.value_title);
-        //console.log('CONTEUDO: ',post.value_content);
-        this.values.unshift( { titulo: post.value_title, conteudo: post.value_content, link: "forum/3" })
-	this.setState( { forum: this.values } )
-    }
+	listarForum() {
+		return this.state.forum.map(function (item_forum) {
+			return (
+
+				<div>
+					<ForumTopic subject={item_forum.titulo} content={item_forum.conteudo} link={item_forum.link} />
+					<br />
+				</div>
+			);
+		})
+	};
 
 
-    render() {
-            return(
-            <div class='container'>
-	   	<h1>Forum</h1>
-	   	<a type="button" class="btn btn-primary btn-lg btn-block" href="/forum/create">add tópico (opção 1)</a>
-	    	<button type="button" class="btn btn-primary btn-lg btn-block" data-toggle="modal" data-target="#exampleModal">add tópico (opção 2)</button>
-	        <br />	
-		{this.listarForum()}
-	    	
-		<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  	     		<div class="modal-dialog" role="document">
-    				<div class="modal-content">
-      					<div class="modal-header">
-        					<h5 class="modal-title" id="exampleModalLabel">Novo tópico</h5>
-        					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          					<span aria-hidden="true">&times;</span>
-        					</button>
-      					</div>
-      					<div class="modal-body">
-        					<ForumTopicCreate update={this.novoPost} />
-      					</div>
-      					<div class="modal-footer">
-        					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      					</div>
-    				</div>
-  			</div>
-  	   	</div>
-           </div>
-        );}
+	novoPost(post) {
+		//console.log('TITULO: ',post.value_title);
+		//console.log('CONTEUDO: ',post.value_content);
+		this.values.unshift({ titulo: post.value_title, conteudo: post.value_content, link: "forum/3" })
+		this.setState({ forum: this.values })
+	}
+
+
+	render() {
+        return(
+            <React.Fragment>
+                {/* TEMP */}
+                <Navbar/>
+			<div class='container'>
+				<h1>Fórum</h1>
+				{/* <a type="button" class="btn btn-primary btn-lg btn-block" href="/forum/create">add tópico (opção 1)</a> */}
+				<button type="button" class="btn btn-primary btn-lg btn-block" data-toggle="modal" data-target="#exampleModal">Adicionar comentário</button>
+				<br />
+				{this.listarForum()}
+
+				<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLabel">Novo tópico</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<div class="modal-body">
+								<ForumTopicCreate update={this.novoPost} />
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			</React.Fragment>
+		);
+	}
 }
 
 export default Forum;
